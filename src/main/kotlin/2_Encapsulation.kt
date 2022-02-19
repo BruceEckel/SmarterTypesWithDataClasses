e244c3b55b8863bfe6e8fbf591187288e605b794
Bruce Eckel <BruceTEckel@gmail.com>
2022-02-18 22:48:00 -0700

    Adjust main()s for better examples

--- src/main/kotlin/2_Encapsulation.kt
+++ src/main/kotlin/2_Encapsulation.kt
@@ -19,18 +19,21 @@ class Stars(n: Int) {
   fun f2(stars: Int): Int {
     condition(stars) // Precondition
     n = stars * 5
-    condition(stars) // Postcondition
+    condition(n) // Postcondition
     return n
   }
   override fun toString() = "Stars($n)"
 }
 
 fun main(args: Array<String>) {
-  val stars1 = Stars(6)
+  val stars1 = Stars(4)
   println(stars1)
   println(stars1.f1(4))
+  println(stars1)
   println(stars1.f2(stars1.f1(4)))
   println(stars1)
   val stars2 = Stars(11)
+  println(stars2)
   println(stars2.f1(3))
+  println(stars2)
 }
--- src/main/kotlin/3_DataClassValidation.kt
+++ src/main/kotlin/3_DataClassValidation.kt
@@ -1,4 +1,6 @@
 // Data classes
+// "A type is a set of values"
+// "Make illegal values unrepresentable"
 import check.range
 
 data class Stars(val n: Int) {
@@ -7,18 +9,21 @@ data class Stars(val n: Int) {
   }
 }
 
-fun f1(stars: Stars) = Stars(stars.n * 10)
+fun f1(stars: Stars) = Stars(stars.n + 5)
 
-fun f2(stars: Stars) = Stars(stars.n + 10)
+fun f2(stars: Stars) = Stars(stars.n * 5)
 
 fun main() {
-  val stars1 = Stars(6)
+  val stars1 = Stars(4)
   // stars1.n = 99;  // Can't modify n
   println(stars1)
   println(f1(stars1))
-  println(f2(stars1))
+  println(f2(f1(stars1)))
+  println(stars1)
   val stars2 = Stars(11)
+  println(stars2)
   println(f1(stars2))
+  println(stars2)
   // Data classes can be keys in hashed data structures,
   // because they define equals() and hashCode():
   val m = mapOf(Stars(4) to "four", Stars(9) to "nine")
